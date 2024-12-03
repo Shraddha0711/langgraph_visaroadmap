bf9068e047d140d071028f93677a69c8823efd67
Shraddha0711 <164492572+Shraddha0711@users.noreply.github.com>
2024-12-03 13:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ async def generate_visa_roadmap(questionnaire:str, roadmap_type:str):
 # Run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app,host= "0.0.0.0"  port=8000)
+    uvicorn.run(app,host= "0.0.0.0", port=8000)
